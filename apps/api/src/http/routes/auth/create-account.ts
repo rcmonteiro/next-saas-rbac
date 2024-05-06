6ad03b30c18457542cf8e24e69172ffe5193e101
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-05-06 09:55:10 -0300

    feat: rota para criar account

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { db } from '@/lib/prisma'
 
 export const createAccount = async (app: FastifyInstance) => {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,30 @@ export const createAccount = async (app: FastifyInstance) => {
         }),
       },
     },
-    () => {
-      return 'User created!'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const useWithSameEmailExists = await db.user.findUnique({
+        where: { email },
+      })
+
+      if (useWithSameEmailExists) {
+        return reply.status(400).send({
+          message: 'User already exists',
+        })
+      }
+
+      const hashedPassword = await hash(password, 6)
+
+      await db.user.create({
+        data: {
+          name,
+          email,
+          passwordHash: hashedPassword,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
