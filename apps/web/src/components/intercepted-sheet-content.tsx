b6f543e03a5bf863554eb4d43a7d5ff59cfd3cc1
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-06-11 10:42:21 -0300

    chore: fix sheet close button style

--- apps/web/src/components/intercepted-sheet-content.tsx
+++ apps/web/src/components/intercepted-sheet-content.tsx
@@ -35,7 +35,8 @@ export const InterceptedSheetContent = React.forwardRef<
       >
         {children}
         <Button
-          variant="icon"
+          size="icon"
+          variant="ghost"
           onClick={() => onDismiss()}
           className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
         >
